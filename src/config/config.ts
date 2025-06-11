c2734359f2960af0070a1a51da3af2216ad57d24
GustavoTadeuBassani <gustavo.tadeu@outlook.com>
2025-06-11 12:02:41 -0300

    title

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 
 export const config = {
-  defaultWebhookUrl: 'https://api.exemplo.com/webhook',
-  templateDownloadUrl: 'https://repositorio.externo.com/template.xlsx',
+  defaultWebhookUrl: 'https://n8noci.gustavotadeu.com.br/webhook/fa8000c5-84a9-420a-a3a1-c91f18cf8027',
+  templateDownloadUrl: 'http://minio.oci.gustavotadeu.com.br:9000/bucket/Template.xlsx',
   maxFileSize: 10 * 1024 * 1024, // 10MB
   allowedFileTypes: [
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
